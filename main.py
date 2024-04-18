278064e6d9f218c49ed2aae8ba7f8d2d4645a04c
Praagya Shandilya <theafrophantom5@gmail.com>
2024-04-18 13:38:51 -0400

    init commit

---
+++ main.py
@@ -0,0 +1,15 @@
+if __name__=="__main__":
+    '''
+        TODO:
+        1.  Build a data structure to store the board state
+            -started 1:36 4/18
+        2.  “Pretty-print” the board to the terminal
+        3.  Given a starting board state, calculate the next one
+        4.  Run the game forever
+        ANCILLARY:
+        1. filesaving mechanism 
+        2. make a terminal ui
+        3. config files for rules 
+    '''
+    print("Hello")
+
